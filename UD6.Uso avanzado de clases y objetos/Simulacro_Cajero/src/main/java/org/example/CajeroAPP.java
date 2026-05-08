44e13fca17ad4b607fd4496adeaf005896e7f2aa
AlexPerse10 <spuknitfire@gmail.com>
2026-05-08 17:26:30 +0200

    proceso de ejercicio simulacion bancaria

--- UD6.Uso avanzado de clases y objetos/Simulacro_Cajero/src/main/java/org/example/CajeroAPP.java
+++ UD6.Uso avanzado de clases y objetos/Simulacro_Cajero/src/main/java/org/example/CajeroAPP.java
@@ -4,18 +4,44 @@ import java.util.Scanner;
 
 public class CajeroAPP {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("=== Cajero automatico ===");
-        System.out.println("[1] Consultar saldo");
-        System.out.println("[2] Ingresar dinero");
-        System.out.println("[3] Retirar dinero");
-        System.out.println("[x] salir");
+        while (true) {
+            CuentaBancaria cuenta1 = new CuentaBancaria();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("=== Cajero automatico ===");
+            System.out.println("[1] Consultar saldo");
+            System.out.println("[2] Ingresar dinero");
+            System.out.println("[3] Retirar dinero");
+            System.out.println("[x] salir");
 
-        System.out.println("selecciona una opcion");
-        char opcion = sc.nextLine().charAt(0);
+            System.out.println("selecciona una opcion");
+            char opcion = sc.nextLine().charAt(0);
 
-        switch ()
+            switch (opcion) {
+                case '1':
+                    cuenta1.mostrarsaldo();
+                    break;
 
+                case '2':
+                    System.out.println("introduce el importe a ingresar");
+                    double ingresar = sc.nextDouble();
+                    cuenta1.ingregardinero(ingresar);
+                    break;
+
+                case '3':
+                    System.out.println("introduce el dinero a retirar");
+                    double ingresar_ = sc.nextDouble();
+                    cuenta1.retirardinero(ingresar_);
+                    break;
+
+                default:
+
+            }
+
+            if (opcion == 'x') {
+                System.out.println("saliendo del menu....");
+                break;
+            }
+        }
 
     }
 }
--- UD6.Uso avanzado de clases y objetos/Simulacro_Cajero/src/main/java/org/example/CuentaBancaria.java
+++ UD6.Uso avanzado de clases y objetos/Simulacro_Cajero/src/main/java/org/example/CuentaBancaria.java
@@ -7,11 +7,15 @@ public class CuentaBancaria {
         this.saldo = 500;
     }
 
-    public double ingregardinero() {
-        return 0;
+    public void ingregardinero(double ingresar) {
+        if (ingresar>3000){
+            
+        }else {
+            setSaldo(ingresar);
+        }
     }
 
-    public double retirardinero() {
+    public double retirardinero(double retirar) {
     return 0;
     }
 
@@ -20,6 +24,7 @@ public class CuentaBancaria {
     }
 
     public void setSaldo(double saldo) {
+
         this.saldo = saldo;
     }
 
