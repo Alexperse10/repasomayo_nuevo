00dcf583986caf05c87669a722b3abeb9982b7f3
AlexPerse10 <spuknitfire@gmail.com>
2026-05-14 18:55:09 +0200

    ejercicios 3

--- UD7. Colecciones/Ejercicios_aules_colecciones/src/main/java/org/example/Ejercicio3_verificarpila.java
+++ UD7. Colecciones/Ejercicios_aules_colecciones/src/main/java/org/example/Ejercicio3_verificarpila.java
@@ -1,16 +1,34 @@
 package org.example;
 
+import java.util.Formattable;
 import java.util.Stack;
 
 public class Ejercicio3_verificarpila {
 
-   static public boolean esbalanceado(String expresion) {
+    static public boolean esbalanceado(String expresion) {
 
         return true;
     }
+
     public static void main(String[] args) {
-        String valor ="(2+3)*(5-1))";
+        String valor = "((2+3)*(5-1))";
         Stack<Character> listacaracteres = new Stack<>();
-       
+
+        for (int i = 0; i < valor.length(); i++) {
+            char valor_caracter = valor.charAt(i);
+            if (valor_caracter == '(') {
+                listacaracteres.push(valor_caracter);
+            }
+            if (valor_caracter==')'){
+              if (listacaracteres.peek() == '(') {
+                  
+              }
+
+                }
+            }
+        }
+
     }
-}
+
+
+
